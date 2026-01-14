1ebc327b583c963f8d032355f57368ef9df238a3
Oleh Kovalenko <oleh.exe.dev@gmail.com>
2026-01-14 23:15:54 +0200

    Make `Mp3StreamTitleConfig` a final class with readonly properties and validation in the constructor.

--- src/Mp3StreamTitleConfig.php
+++ src/Mp3StreamTitleConfig.php
@@ -2,7 +2,28 @@
 
 namespace Mp3StreamTitle;
 
-class Mp3StreamTitleConfig
+final class Mp3StreamTitleConfig
 {
+    public readonly int $sendType = Mp3StreamTitle::SEND_CURL;
+    public readonly string $userAgent;
+    public readonly bool $showErrors;
+    public readonly int $metaMaxLength;
 
+    public function __construct(
+        string $userAgent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.75 Safari/537.36',
+        bool $showErrors = false,
+        int $metaMaxLength = 4080
+    ) {
+        if ($userAgent === '') {
+            throw new InvalidArgumentException('User-Agent cannot be empty');
+        }
+
+        if ($metaMaxLength > 4080) {
+            throw new InvalidArgumentException('metaMaxLength must be no more than 4080 bytes');
+        }
+
+        $this->userAgent = $userAgent;
+        $this->showErrors = $showErrors;
+        $this->metaMaxLength = $metaMaxLength;
+    }
 }
