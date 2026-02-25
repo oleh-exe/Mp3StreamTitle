d1d7765a2ebb068085f0b33810d3167638cdf5c7
Oleh Kovalenko <oleh.exe.dev@gmail.com>
2026-02-25 23:47:02 +0200

    Make `CurlHttpClient` class `readonly` and add streaming functionality with customizable options and headers

--- src/Infrastructure/Http/CurlHttpClient.php
+++ src/Infrastructure/Http/CurlHttpClient.php
@@ -19,7 +19,108 @@ declare(strict_types=1);
 
 namespace Mp3StreamTitle\Infrastructure\Http;
 
-class CurlHttpClient
-{
+use Closure;
 
+readonly class CurlHttpClient
+{
+    /**
+     * @var array
+     */
+    private array $options;
+
+    /**
+     * @var array
+     */
+    private array $headers;
+
+    /**
+     * @var int
+     */
+    private int $seconds;
+
+    /**
+     * @var Closure
+     */
+    private Closure $callback;
+
+    /**
+     * @var string
+     */
+    private string $userAgent;
+
+    /**
+     * @var string
+     */
+    private string $streamingUrl;
+
+    /**
+     * @param array $options
+     */
+    public function __construct(
+        array $options = [],
+        //string $streamingUrl,
+        //string $userAgent,
+        //Closure $callback
+    )
+    {
+        //$this->streamingUrl = $streamingUrl;
+        //$this->userAgent = $userAgent;
+        //$this->callback = $callback;
+        $this->options = $options;
+    }
+
+    public function getStream(
+        string $url,
+        callable $onChunk
+    ): void {
+        // Initialize the cURL session.
+        $ch = curl_init();
+
+        // Set the parameters for the session.
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 30);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, ['icy-metadata: 1']);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+        curl_setopt($ch, CURLOPT_USERAGENT, $this->options['userAgent']);
+        curl_setopt($ch, CURLOPT_WRITEFUNCTION, $onChunk);
+
+        // Execute the request.
+        curl_exec($ch);
+
+        // If there are errors we save them into variables.
+        $errno = curl_errno($ch);
+        $error = curl_error($ch);
+
+        // End the session.
+        curl_close($ch);
+    }
+
+    /**
+     * @param array $headers
+     * @return void
+     */
+    public function setHeaders(array $headers): void
+    {
+        $this->headers = $headers;
+    }
+
+    /**
+     * @param int $seconds
+     * @return void
+     */
+    public function setTimeout(int $seconds): void
+    {
+        $this->seconds = $seconds;
+    }
+
+    /**
+     * @return void
+     */
+    public function close(): void
+    {
+    }
 }
