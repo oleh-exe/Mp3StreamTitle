b669d4587d9c80af51c2ba0c265b847e4371b6f7
Oleh Kovalenko <oleh.exe.dev@gmail.com>
2026-04-19 21:18:24 +0300

    Introduce `MetadataExtractor` to parse and extract ICY metadata from HTTP stream bodies

---
+++ src/Infrastructure/Http/MetadataExtractor.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * Copyright 2026 Oleh Kovalenko
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+declare(strict_types=1);
+
+namespace Mp3StreamTitle\Infrastructure\Http;
+
+use RuntimeException;
+
+final class MetadataExtractor
+{
+    /**
+     * @param string $body
+     * @param int $offset
+     *
+     * @return string
+     */
+    public function extract(string $body, int $offset): string
+    {
+        if (strlen($body) <= $offset) {
+            throw new RuntimeException(
+                'Stream body is shorter than metadata offset'
+            );
+        }
+
+        if (!isset($body[$offset])) {
+            throw new RuntimeException(
+                'Metadata offset is out of bounds'
+            );
+        }
+
+        // ICY metadata block structure:
+        // [offset]      = length byte (metadata length / 16)
+        // [offset + 1]  = start of actual metadata (e.g. StreamTitle='...';)
+        $metaStart = $offset + 1;
+
+        // Find out the length of metadata.
+        $metaLength = ord($body[$offset]) * 16;
+
+        if ($metaLength === 0) {
+            return '';
+        }
+
+        if (strlen($body) < ($metaStart + $metaLength)) {
+            throw new RuntimeException(
+                'Incomplete metadata block received from stream'
+            );
+        }
+
+        // Get metadata in the following format "StreamTitle='artist name and song name';".
+        return substr(
+            $body,
+            $metaStart,
+            $metaLength
+        );
+    }
+}
